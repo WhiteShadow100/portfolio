9d8094b914e0a9491ccbe1f9a2db6a675dff2518
WhiteShadow100 <jmaharjan4@gmail.com>
2023-08-01 18:40:47 +0545

    adding axios call

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,9 +6,19 @@ import { Link } from 'react-router-dom'
 import AppRouter from './AppRouter'
 
 import './style/canvas.css';
+import axios from 'axios'
 
 function App() {
   
+  useEffect(() => {
+    axios.get('https://whiteshadow.bsite.net/WeatherForecast')
+      .then(response => {
+        console.log("Response => ", response  )
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }, []);
 
   return (
     <>
