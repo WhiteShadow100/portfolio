4da947a1e01a693647db1b73b4b9c76bd4a9865d
WhiteShadow100 <jmaharjan4@gmail.com>
2023-11-04 09:04:37 +0545

    commented built in css temporarly and created component for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+// CSS necssary for snake game
+// import './App.css'
+
 import { Link } from 'react-router-dom'
 import AppRouter from './AppRouter'
 
 import './style/canvas.css';
 import SnakeGame from './component/games/snake_game/SnakeGame'
+import Header from './component/header/Header'
 
 function App() {
   
 
   return (
     <>
+      {/* <Header /> */}
       {/* <AppRouter /> */}
       <SnakeGame width={400} height={300} />
     </>
